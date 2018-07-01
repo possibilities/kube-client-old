f306c495d3a73a4de752cf1e5160e81841195be2
Mike Bannister <notimpossiblemike@gmail.com>
2018-07-01 13:40:59 -0400

    Reintroduce exit hook
    Initial problem was SIGHUP, seems unintrusive enough now to bring
    back.

--- startProxy.js
+++ startProxy.js
@@ -1,6 +1,7 @@
 const { spawn } = require('child-process-promise')
 const { findAPortNotInUse } = require('portscanner')
 const { address: getIp } = require('ip')
+const exitHook = require('exit-hook')
 
 const startProxy = async () => {
   const ip = getIp()
@@ -9,18 +10,20 @@ const startProxy = async () => {
   const [cmd, ...args] = command.split(' ')
   const proxying = spawn(cmd, args)
 
+  const disconnect = () => {
+    proxying.catch(e => {})
+    proxying.childProcess.kill()
+  }
+
+  exitHook(disconnect)
+
   return new Promise(resolve => {
     let output = ''
     proxying.childProcess.stdout.on('data', data => {
       output = `${output}${data.toString()}`
       if (output.includes(`Starting to serve on ${ip}:${port}`)) {
-        resolve({
-          disconnect: () => {
-            proxying.catch(e => {})
-            proxying.childProcess.kill()
-          },
-          config: { baseUrl: `http://${ip}:${port}` }
-        })
+        const config = { baseUrl: `http://${ip}:${port}` }
+        resolve({ disconnect, config })
       }
     })
   })
