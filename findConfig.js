38eeba5dee134f7274803dd0f85f1ca4b298b544
Mike Bannister <notimpossiblemike@gmail.com>
2018-07-23 22:23:41 -0400

    Generalize config based on `~/.kube/config`

--- findConfig.js
+++ findConfig.js
@@ -1,7 +1,8 @@
 const { join: joinPath } = require('path')
-const { readFile, exists } = require('fs-extra')
 const { exec } = require('child-process-promise')
+const { readFile, exists } = require('fs-extra')
 const { safeLoad: readYaml } = require('js-yaml-promise')
+const get = require('lodash/get')
 
 const host = process.env.KUBERNETES_SERVICE_HOST
 const port = process.env.KUBERNETES_SERVICE_PORT
@@ -11,7 +12,27 @@ const tokenPath = '/var/run/secrets/kubernetes.io/serviceaccount/token'
 
 const decode = encoded => Buffer.from(encoded, 'base64').toString()
 
+const keyForUser = user => {
+  if (get(user, 'client-key')) {
+    return readFile(get(user, 'client-key'), 'utf8')
+  }
+  if (get(user, 'client-key-data')) {
+    return decode(get(user, 'client-key-data'))
+  }
+}
+
+const certForUser = user => {
+  if (get(user, 'client-certificate')) {
+    return readFile(get(user, 'client-certificate'), 'utf8')
+  }
+  if (get(user, 'client-certificate-data')) {
+    return decode(get(user, 'client-certificate-data'))
+  }
+}
+
 const findConfig = async () => {
+  // If we're inside kubernetes we can pull all the values we need from
+  // the filesystem
   if (await exists(caPath) && await exists(tokenPath)) {
     return {
       baseUrl: `https://${host}:${port}`,
@@ -20,43 +41,32 @@ const findConfig = async () => {
     }
   }
 
+  // If we're outside of kubernetes we manually parse out everything we need
+  // from ~/.kube/config
   const configPath = joinPath(process.env.HOME, '.kube', 'config')
   const configString = await readFile(configPath, 'utf8')
   const config = await readYaml(configString)
   const currentContextName = config['current-context']
   const currentContext = config.contexts.find(context => context.name === currentContextName)
   const cluster = config.clusters.find(cluster => cluster.name === currentContext.context.cluster).cluster
-
-  // TODO A bit brute, can probably find these values through ~/.kube/config
-  // more naturally
-  if (currentContextName === 'minikube') {
-    const user = config.users.find(user => user.name === currentContext.context.user).user
-    return {
-      baseUrl: cluster.server,
-      cert: await readFile(user['client-certificate'], 'utf8'),
-      key: await readFile(user['client-key'], 'utf8'),
-      ca: await readFile(cluster['certificate-authority'], 'utf8')
-    }
-  }
+  const user = get(config.users.find(user => user.name === currentContext.context.user), 'user')
 
   const { stdout: secretString } = await exec('kubectl get secret -ojson')
   const { items: secrets } = JSON.parse(secretString)
-  const defaultSecret = secrets.find(secret => secret.metadata.name.startsWith('default-token-'))
+  const { token } = secrets.find(secret => secret.metadata.name.startsWith('default-token-')).data
 
-  if (currentContextName === 'docker-for-desktop') {
-    const user = config.users.find(user => user.name === currentContext.context.user).user
-    return {
-      baseUrl: cluster.server,
-      rejectUnauthorized: false,
-      cert: decode(user['client-certificate-data']),
-      key: decode(user['client-key-data']),
-      headers: { authorization: `Bearer ${defaultSecret.data.token}` }
-    }
-  }
-
+  // Dynamic config based on current context
   return {
     baseUrl: cluster.server,
-    headers: { authorization: `Bearer ${defaultSecret.data.token}` }
+    rejectUnauthorized: !(cluster['insecure-skip-tls-verify'] && cluster.server.startsWith('https')),
+    key: await keyForUser(user),
+    cert: await certForUser(user),
+    ca: cluster['certificate-authority']
+      ? await readFile(cluster['certificate-authority'], 'utf8')
+      : undefined,
+    headers: cluster['certificate-authority']
+      ? undefined
+      : { authorization: `Bearer ${token}` }
   }
 }
 
