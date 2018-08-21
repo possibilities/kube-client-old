5909cc784e55e2fc152104ab49acb2d0f8233279
Mike Bannister <possibilities@users.noreply.github.com>
2018-08-20 22:34:42 -0400

    Fix https agent config (#12)

--- getInClusterConfig.js
+++ getInClusterConfig.js
@@ -1,4 +1,5 @@
 const { readFile, exists } = require('fs-extra')
+const https = require('https')
 
 const host = process.env.KUBERNETES_SERVICE_HOST
 const port = process.env.KUBERNETES_SERVICE_PORT
@@ -10,12 +11,11 @@ const getInClusterConfig = async () => {
   // If we're inside kubernetes we can pull all the values we need from
   // the filesystem
   if (host && port && await exists(caPath) && await exists(tokenPath)) {
+    const agent = new https.Agent({ ca: await readFile(caPath, 'utf8') })
     return {
+      agent,
       baseUrl: `https://${host}:${port}`,
-      headers: { authorization: `Bearer ${await readFile(tokenPath, 'utf8')}` },
-      agentOptions: {
-        ca: await readFile(caPath, 'utf8')
-      }
+      headers: { authorization: `Bearer ${await readFile(tokenPath, 'utf8')}` }
     }
   }
 }
